a349e035d2ebba87be03b7bc4138a032380310cd
aridepai17 <advaithdepai26@gmail.com>
2025-12-03 01:19:26 +0530

    feat(navbar): hook mode icon to theme toggle
    • Navbar imports useThemeStore and calls toggleTheme when mode.svg (id 4) clicked.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { navLinks, navIcons } from "#constants";
+import useThemeStore from "#store/theme";
 import useWindowStore from "#store/window";
 import dayjs from "dayjs";
 
 const Navbar = () => {
 	const { openWindow } = useWindowStore();
+    const { toggleTheme } = useThemeStore();
 
 	return (
 		<nav>
@@ -22,15 +24,21 @@ const Navbar = () => {
 
 			<div>
 				<ul>
-					{navIcons.map(({ id, img }) => (
-						<li key={id}>
-							<img
-								src={img}
-								className="icon-hover"
-								alt={`icon-${id}`}
-							/>
-						</li>
-					))}
+					{navIcons.map(({ id, img }) =>
+                        id === 4 ? (
+                            <li key={id} onClick={toggleTheme}>
+                                <img src={img} className="icon-hover" alt="mode-toggle" />
+                            </li>
+                        ) : (
+                            <li key={id}>
+                                <img
+                                    src={img}
+                                    className="icon-hover"
+                                    alt={`icon-${id}`}
+                                />
+                            </li>
+                        )
+                    )}
 				</ul>
 
 				<time className="font-geist font-black">{dayjs().format("ddd MMM D h:mm A")}</time>
